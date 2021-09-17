ada03c94d54d5aaaf7d9335e1a29fdfd80eb54c0
Jeremy Wallace <jeremygw@amazon.com>
2021-09-17 17:27:24 -0400

    standardize the build and upload script

--- build_and_upload.sh
+++ build_and_upload.sh
@@ -1,4 +1,8 @@
 #!/bin/bash
-aws ecr get-login-password --region us-west-2 | docker login --username AWS --password-stdin 593875212637.dkr.ecr.us-west-2.amazonaws.com
-docker buildx build . -t 593875212637.dkr.ecr.us-west-2.amazonaws.com/move-test:latest --platform linux/arm64 --push
-aws s3 cp docker-compose.yaml s3://ros-dance-greengrass-demo/move-test/latest/artifacts/docker-compose.yaml
+account=<ACCOUNT>
+region=<REGION>
+s3_key=s3://<BUCKET_NAME>/<PATH>
+container_name=jetbot
+aws ecr get-login-password --region $region| docker login --username AWS --password-stdin $account.dkr.ecr.$region.amazonaws.com
+docker buildx build . -t $account.dkr.ecr.$region.amazonaws.com/$container_name:latest --platform linux/arm64 --push
+aws s3 cp docker-compose.yaml $s3_key/docker-compose.yaml
